29ae59da9179304fe161cbe8ab49eee1f6173a96
ComandPromt <vayase_a_ferrol@hotmail.com>
2021-12-28 13:13:05 +0100

    Update index.php

--- Server/index.php
+++ Server/index.php
@@ -1,49 +1,79 @@
-<?php      
-
+<?php  
+    
 mkdir("imagenes",0777);
 
 if (isset($_FILES['archivo'])) {
+	
     $file = $_FILES["archivo"];
-    $tipo = $file['type'];
-    $path = $file['tmp_name'];
-    $size = $file['size'];
-    $nombre = $file['name'];
-    $dimensiones = getimagesize($path);
-    $width = $dimensiones[0];
-    $height = $dimensiones[1];
-    $carpeta = "imagenes/";
-    $imagen = NULL;
-    $urlImagen = NULL;
+    
+	$tipo = $file['type'];
+    
+	$path = $file['tmp_name'];
+    
+	$size = $file['size'];
+    
+	$nombre = $file['name'];
+    
+	$dimensiones = getimagesize($path);
+    
+	$width = $dimensiones[0];
+    
+	$height = $dimensiones[1];
+    
+	$carpeta = "imagenes/";
+    
+	$imagen = NULL;
+    
+	$urlImagen = NULL;
+	
     if ($tipo == "image/jpg" || $tipo == "image/jpeg" || $tipo == "image/JPG" || $tipo == "image/JPEG") {
+		
         $imagen = imagecreatefromjpeg($path);
-    } else if ($tipo == "image/png" || $tipo == "image/PNG") {
+		
+    }
+
+	else if ($tipo == "image/png" || $tipo == "image/PNG") {
+		
         $imagen = imagecreatefrompng($path);
-    } else {
+		
+    }
+
+	else {
+		
         echo 'FORMATO DE ARCHIVO INVÁLIDO';
+		
     }
 	
     if (is_uploaded_file($path) && $imagen != NULL) {
+						
+        $urlImagen = $carpeta . "$nombre";
 		
-        $tipo = str_replace("image/", "", $tipo);
-		
-        if (isset($_POST['imgname'])) {
-            $nombre = $_POST['imgname'];
-        } else {
-            $nombre = str_replace("." . $tipo, "", $nombre);
-        }
-		
-        $urlImagen = $carpeta . "$nombre.$tipo";
         $image_p = imagecreatetruecolor($width, $height);
-        imagecopyresampled($image_p, $imagen, 0, 0, 0, 0, $width, $height, $width, $height);
-        $uploaded = imagejpeg($image_p, $urlImagen);
 		
+        imagecopyresampled($image_p, $imagen, 0, 0, 0, 0, $width, $height, $width, $height);
+		
+        $uploaded = imagejpeg($image_p, $urlImagen);
+				
         if ($uploaded == TRUE) {
+			
             echo 'IMAGEN SUBIDA CORRECTAMENTE';
-        } else {
+			
+        } 
+		
+		else {
+			
             echo 'ERROR AL SUBIR IMAGEN';
+			
         }
+		
     }
-} else {
+	
+} 
+
+else {
+	
     echo 'NO HAY IMAGEN';
+	
 }
-      
+
+?>
